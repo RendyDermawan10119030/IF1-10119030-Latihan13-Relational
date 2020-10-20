0acecc1452fc3227c83a5690acb0249dfe4b5424
RendyDermawan10119030 <rendy.10119030@mahasiswa.unikom.ac.id>
2020-10-20 22:54:17 +0700

    First commit

---
+++ src/if1/pkg10119030/latihan13/relational/IF110119030Latihan13Relational.java
@@ -0,0 +1,19 @@
+package if1.pkg10119030.latihan13.relational;
+/*
+ * Nama     : Rendy Dermawan
+ * Kelas    : IF-1
+ * NIM      : 10119030
+ * Deskripsi Program :Latihan Relational
+ */    
+    public class IF110119030Latihan13Relational {
+        public static void main(String[] args) {
+            int bil1 = 10;
+            int bil2 = 20;
+            System.out.println("a == b = " + (bil1 == bil2));
+            System.out.println("a != b = " + (bil1 != bil2));
+            System.out.println("a > b = " + (bil1 > bil2));
+            System.out.println("a < b = " + (bil1 < bil2));
+            System.out.println("b >= a = " + (bil1 >= bil2));
+            System.out.println("b <= a = " + (bil1 <= bil2));
+        }   
+}
